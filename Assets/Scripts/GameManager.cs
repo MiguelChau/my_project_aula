90efb59e3bae876b1260f3ef5ce75a7d27a0c857
MiguelChau <jmchaupeixoto@gmail.com>
2023-03-17 00:20:38 +0000

    adding GM and Singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Core.Singleton;
+using DG.Tweening;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Players")]
+    public List<GameObject> playerPrefab;
 }
