7a9fe6fac6b1d68329af49cd214c39270c290967
MiguelChau <jmchaupeixoto@gmail.com>
2023-03-20 01:09:26 +0000

    animação de Scale no player

--- Assets/Scripts/Player/PlayerBase.cs
+++ Assets/Scripts/Player/PlayerBase.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerBase : MonoBehaviour
 {
     public Rigidbody2D myRigidBody;
 
+    [Header("Speed")]
     public Vector2 friction = new Vector2(-.1f, 0);
     public float speed;
     public float speedRun;
     public float jumpForce = 2;
 
+    [Header("Animation")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 1f;
+    public float animationDuration = .5f;
+    public Ease ease = Ease.OutBack;
 
     private float _currentSpeed;
 
@@ -51,7 +58,19 @@ public class PlayerBase : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             myRigidBody.velocity = Vector2.up * jumpForce;
+            //usado para que não buggue o scale!//
+            myRigidBody.transform.localScale = Vector2.one; 
+
+            DOTween.Kill(myRigidBody.transform);
+
+            HandleScale();
         }
+    }
+
+    private void HandleScale()
+    {
+        myRigidBody.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidBody.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
    
 }
